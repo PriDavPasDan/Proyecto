b047907adeb963406c85f326eaaae06fafc8634b
PriDavPasDan <davidcool10@hotmail.com>
2016-11-07 20:22:21 -0700

    Add files via upload

--- PROYECTO_FINAL/src/OrigenDestino/EsquinaNorOeste.java
+++ PROYECTO_FINAL/src/OrigenDestino/EsquinaNorOeste.java
@@ -16,7 +16,22 @@ public class EsquinaNorOeste {
         int[][] aTabla = new int[aOferta.length][aDemanda.length];
         //VARIABLE DEL COSTO
         int iCostoTotal = 0;
+        
+        //IMPRIMIR LA TABLA INICIAL
+         System.out.println("TABLA INICIAL");
         for (int i = 0; i < aOferta.length; i++) {
+            System.out.println("Oferta:" + aOferta[i]);
+            for (int j = 0; j < aDemanda.length; j++) {
+                System.out.print(" [" + aTabla[i][j] + "]");
+                System.out.println("\t Demanda:" + aDemanda[j]);
+            }
+        }
+        
+        //PROCESO PARA ASIGNAR LOS DATOS
+        System.out.println(" ");
+        System.out.println("TABLA ASIGNANDO DATOS");
+        for (int i = 0; i < aOferta.length; i++) {
+            System.out.print("Oferta:" + aOferta[i]+"\t");
             for (int j = 0; j < aDemanda.length; j++) {
                 //CONDICIONAL SI LA OFERTA ES MAYOR O IGUAL QUE LA DEMANDA
                 if(aOferta[i] >= aDemanda[j]){
@@ -37,10 +52,22 @@ public class EsquinaNorOeste {
                     //AL AGOTARNOS LA OFERTA ESTA SE VUELVE 0
                     aOferta[i] = 0;
                 }
-                //ENCONTRAR LA FORMA DE IMPRIMIR LA TABLA
+                //IMPRIMIR LA TABLA LLENA
+                System.out.print("[" + aTabla[i][j] + "]");
+                System.out.print("\tDemanda:" + aDemanda[j] + "\t");
             }
+            System.out.println(" ");
         }
-        //ENCONTRAR LA FORMA DE CALCULAR EL MINIMO COSTO DE ENVIO
-         return iCostoTotal;
+        //CALCULAR EL COSTO A TRAVES DE LOS PRECIOS
+        for (int i = 0; i <= 2; i++) {
+             for (int j = 0; j <= 2; j++) {
+                 iCostoTotal = iCostoTotal + aCosto[i][j]*aTabla[i][j];
+             }
+         }
+        //IMPRIMIR EL COSTO TOTAL MINIMO
+        System.out.println(" ");
+        System.out.println("Costo Total: " + iCostoTotal);
+        return iCostoTotal;
     }
-}
+}    
+       
